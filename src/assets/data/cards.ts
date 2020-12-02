49bbd8a56c9d73fca7dc33566d180f06650c4cfe
Leonard Tng <leonardtng@gmail.com>
2020-12-03 04:02:23 +0800

    Typo in codecache card

--- src/assets/data/cards.ts
+++ src/assets/data/cards.ts
@@ -170,7 +170,7 @@ export const cards: Array<CardData> = [
     date: `November 2020`,
     role: `The lightweight code snippet storage built for developers and learners`,
     description: `My first full-stack project I designed, built and deployed during my first semester in college consisting of both a frontend and a backend. Initially, 
-    this project started off as a simple idea for a site that can store my code snippets. Eventually I saw this idea through to build the final product, complete 
+    this project started off as a simple idea for a site that can store my code snippets. I eventually saw this idea through to build the final product, complete 
     with its own authentication system and fully functional code editor in a clean and simple UI. This project has allowed me to 
     improve on both my frontend and backend skills, as well as important development tools such as AWS EC2, Lambda, and Docker.`,
     projectLink: `https://www.codecache.io`,
